afb393985756724ae32869fc20c2add065f6eae4
Andrey Harutyunyan <kaspersky1236@gmail.com>
2025-01-16 19:18:33 +0400

    finally fixing bags :)

--- studybuddy/base/urls.py
+++ studybuddy/base/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('rooms/', views.room, name="room"),
-    path('rooms/<int:r_id>/', views.rooms, name="rooms"),
 ]
 
 
--- studybuddy/base/views.py
+++ studybuddy/base/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Lets learn js'},
+    {'id': 3, 'name': 'Lets learn java'},
+]
+
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "base/home.html")
 
 def room(request):
-    return render(request, "room.html")
-
-def rooms(request, r_id):
-    return render(request, "rooms.html", {
-        "r_id": r_id
+    return render(request, "base/room.html", {
+        'rooms': rooms
     })
+
+
+
+
 
