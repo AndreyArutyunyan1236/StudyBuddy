7e47b421f09ef29f738f00b5dc221d2cad7e40bc
Andrey Harutyunyan <kaspersky1236@gmail.com>
2025-01-21 13:50:30 +0400

    work with admin panel, creating rooms using admin (Changed models, views, admin).

--- studybuddy/base/views.py
+++ studybuddy/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 
 rooms = [
     {'id': 1, 'name': 'Lets learn python'},
@@ -9,20 +9,19 @@ rooms = [
 
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, "base/home.html", {
-        'rooms': rooms
+        'rooms': rooms,
     })
 
+
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    
+    room = Room.objects.get(id=pk)
 
     context = {'room': room}
-
-
     return render(request, "base/room.html", context)
+
 
 
 
